fc4c4411ade126acf141f7c5cb80adfcfe520606
Timmy <140733715+timmy277@users.noreply.github.com>
2024-09-25 18:44:34 +0700

    alter env

--- index.js
+++ index.js
@@ -10,22 +10,12 @@ const router = require('./routes');
 const app = express()
 app.use(cors({
     origin: process.env.FRONTEND_URL,
-    credentials: true,
-    preflightContinue: false,
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    methods: ['GET,HEAD,PUT,PATCH,POST,DELETE'],
+    credentials: true
 }))
-
-app.options('*', cors());
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser()) 
 
-app.use((req, res, next) => {
-    // res.header('Access-Control-Allow-Origin', '*');
-    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
-    next();
-});
 
 app.use("/api",router)
 
