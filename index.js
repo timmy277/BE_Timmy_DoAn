bb067c57bc204820c7d75d96831684cc2c645d41
Timmy <140733715+timmy277@users.noreply.github.com>
2024-09-25 16:16:53 +0700

    alter cors

--- index.js
+++ index.js
@@ -8,22 +8,31 @@ const connectDB = require('./db');
 const router = require('./routes');
 
 const app = express()
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-}))
+
+app.use(
+    cors({
+        origin: '*',
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        preflightContinue: false,
+    }),
+);
+app.options('*', cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
-app.use(cookieParser()) 
+app.use(cookieParser())
 
 
-app.use("/api",router)
+app.use("/api", router)
 
-const PORT = process.env.PORT ||  8080
+const PORT = process.env.PORT || 8080
 
-connectDB().then(()=>{
+connectDB().then(() => {
     app.listen(PORT, () => {
         console.log('MongoDB connected successfully');
         console.log(`Example app listening on port ${PORT}`)
-    })  
+    })
 })
